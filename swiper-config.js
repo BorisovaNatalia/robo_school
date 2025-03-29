6b4be2d6f7744ec6d146c96a371cbdbbd0372788
Natalia Borisova <borisovan.work@gmail.com>
2025-03-29 13:33:24 -0400

    part2

--- swiper-config.js
+++ swiper-config.js
@@ -1,61 +1,61 @@
-const content = document.querySelector('.scroll-content');
-const thumb = document.querySelector('.scroll-thumb');
-const scrollbar = document.querySelector('.custom-scrollbar');
-const btnLeft = document.getElementById('scroll-left');
-const btnRight = document.getElementById('scroll-right');
+// const content = document.querySelector('.scroll-content');
+// const thumb = document.querySelector('.scroll-thumb');
+// const scrollbar = document.querySelector('.custom-scrollbar');
+// const btnLeft = document.getElementById('scroll-left');
+// const btnRight = document.getElementById('scroll-right');
 
-let isDragging = false;
-let startX;
-let startScrollLeft;
+// let isDragging = false;
+// let startX;
+// let startScrollLeft;
 
-/* Обновляем положение ползунка */
-function updateThumbPosition() {
-    const scrollWidth = content.scrollWidth - content.clientWidth;
-    const thumbWidth = (content.clientWidth / content.scrollWidth) * scrollbar.clientWidth;
-    const thumbPosition = (content.scrollLeft / scrollWidth) * (scrollbar.clientWidth - thumbWidth);
+// /* Обновляем положение ползунка */
+// function updateThumbPosition() {
+//     const scrollWidth = content.scrollWidth - content.clientWidth;
+//     const thumbWidth = (content.clientWidth / content.scrollWidth) * scrollbar.clientWidth;
+//     const thumbPosition = (content.scrollLeft / scrollWidth) * (scrollbar.clientWidth - thumbWidth);
 
-    thumb.style.width = `${thumbWidth}px`;
-    thumb.style.transform = `translateX(${thumbPosition}px)`;
-}
+//     thumb.style.width = `${thumbWidth}px`;
+//     thumb.style.transform = `translateX(${thumbPosition}px)`;
+// }
 
-/* Прокрутка по кнопкам */
-btnLeft.addEventListener('click', () => {
-    content.scrollLeft -= 300;
-    setTimeout(updateThumbPosition, 100);
-});
+// /* Прокрутка по кнопкам */
+// btnLeft.addEventListener('click', () => {
+//     content.scrollLeft -= 300;
+//     setTimeout(updateThumbPosition, 100);
+// });
 
-btnRight.addEventListener('click', () => {
-    content.scrollLeft += 300;
-    setTimeout(updateThumbPosition, 100);
-});
+// btnRight.addEventListener('click', () => {
+//     content.scrollLeft += 300;
+//     setTimeout(updateThumbPosition, 100);
+// });
 
-/* Перетаскивание скроллбара */
-thumb.addEventListener('mousedown', (e) => {
-    isDragging = true;
-    startX = e.clientX;
-    startScrollLeft = content.scrollLeft;
-    thumb.style.cursor = 'grabbing';
-});
+// /* Перетаскивание скроллбара */
+// thumb.addEventListener('mousedown', (e) => {
+//     isDragging = true;
+//     startX = e.clientX;
+//     startScrollLeft = content.scrollLeft;
+//     thumb.style.cursor = 'grabbing';
+// });
 
-/* Перемещение мыши */
-document.addEventListener('mousemove', (e) => {
-    if (!isDragging) return;
-    const deltaX = e.clientX - startX;
-    const scrollWidth = content.scrollWidth - content.clientWidth;
-    const thumbWidth = (content.clientWidth / content.scrollWidth) * scrollbar.clientWidth;
-    const maxThumbPos = scrollbar.clientWidth - thumbWidth;
+// /* Перемещение мыши */
+// document.addEventListener('mousemove', (e) => {
+//     if (!isDragging) return;
+//     const deltaX = e.clientX - startX;
+//     const scrollWidth = content.scrollWidth - content.clientWidth;
+//     const thumbWidth = (content.clientWidth / content.scrollWidth) * scrollbar.clientWidth;
+//     const maxThumbPos = scrollbar.clientWidth - thumbWidth;
 
-    const newScrollLeft = startScrollLeft + (deltaX / maxThumbPos) * scrollWidth;
-    content.scrollLeft = newScrollLeft;
-    updateThumbPosition();
-});
+//     const newScrollLeft = startScrollLeft + (deltaX / maxThumbPos) * scrollWidth;
+//     content.scrollLeft = newScrollLeft;
+//     updateThumbPosition();
+// });
 
-/* Отпускание мыши */
-document.addEventListener('mouseup', () => {
-    isDragging = false;
-    thumb.style.cursor = 'grab';
-});
+// /* Отпускание мыши */
+// document.addEventListener('mouseup', () => {
+//     isDragging = false;
+//     thumb.style.cursor = 'grab';
+// });
 
-/* Обновляем положение ползунка при скролле */
-content.addEventListener('scroll', updateThumbPosition);
-updateThumbPosition();
+// /* Обновляем положение ползунка при скролле */
+// content.addEventListener('scroll', updateThumbPosition);
+// updateThumbPosition();
